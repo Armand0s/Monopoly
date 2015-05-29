cf49dad4aee5a3e7bc6d3acea2f83102cf7ae119
souchoar <souchoar@pc-dg-037-14.iut2.upmf-grenoble.fr>
2015-05-29 12:14:04 +0200

    Ajout attribut proprio / get et set

--- src/Jeu/Compagnie.java
+++ src/Jeu/Compagnie.java
@@ -1,8 +1,23 @@
 package Jeu;
 
 public class Compagnie extends CarreauPropriete {
+    
+    private Joueur proprietaire;
         
         public Compagnie(int numero, String nomCarreau, Monopoly monopoly, int prixAchat){
             super(numero, nomCarreau, monopoly, prixAchat);
         }
+
+    @Override
+    public void setProprietaire(Joueur proprietaire) {
+        this.proprietaire = proprietaire; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Joueur getProprietaire() {
+        return this.proprietaire; //To change body of generated methods, choose Tools | Templates.
+    }
+        
+        
+        
 }
