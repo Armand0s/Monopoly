066f390a875dec9bc10cce55bfa6fcad5a1413d0
Armand <Armand@Armand-PP>
2015-05-29 21:00:45 +0200

    Création carte

---
+++ src/Jeu/CarteChance.java
@@ -0,0 +1,12 @@
+package Jeu;
+
+public enum CarteChance {
+	avancer_3, 
+        reculer_3, 
+        rue_Paix, 
+        case_depart, 
+        libere_prison,
+        payer_50,
+        payer_100,
+        payer_75
+}
