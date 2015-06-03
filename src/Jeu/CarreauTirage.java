f0cc1164812d3625f7567aa1ac0a56745c51af8a
Bat <Bat@Battuc>
2015-06-03 14:37:08 +0200

    Mise au point de la méthode servant à "Tirer une carte" dans CarreauTirage.

--- src/Jeu/CarreauTirage.java
+++ src/Jeu/CarreauTirage.java
@@ -1,10 +1,35 @@
 package Jeu;
 
+import static Jeu.CarteChanceEnum.libere_prison;
+import static Jeu.CarteCaisseEnum.libere_prison;
+
 public class CarreauTirage extends CarreauAction {
 
     public CarreauTirage(int numero, String nomCarreau, Monopoly monopoly) {
         super(numero, nomCarreau, monopoly);
     }
     
+    public void tirerCarte(){
+        if (this.getNomCarreau() == "Caisse de Communauté"){
+            CarteCaisse carteATirer = this.getMonopoly().getCarteCaisses().pollFirst();
+            carteATirer.actionAEffectuer();
+            if (carteATirer.getAction() != CarteCaisseEnum.libere_prison){
+                this.getMonopoly().getCarteCaisses().add(carteATirer);
+            }
+            else {
+                this.getMonopoly().getCurrentPlayer().setCarteCaisseLibere(true);
+            }
+        }
+        else {
+            CarteChance carteATirer = this.getMonopoly().getCarteChances().pollFirst();
+            carteATirer.actionAEffectuer();
+            if (carteATirer.getAction() != CarteChanceEnum.libere_prison){
+                this.getMonopoly().getCarteChances().add(carteATirer);
+            }
+            else {
+                this.getMonopoly().getCurrentPlayer().setCarteChanceLibere(true);
+            }
+        }
+    }
     
 }
