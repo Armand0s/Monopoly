cc2bbaccfc3ddb10565b874aa5c3d9c449bc8731
zombini <zombini@wificampus-044164.grenet.fr>
2015-06-08 14:19:44 +0200

    Documentation de certaine fonction

--- src/Jeu/Monopoly.java
+++ src/Jeu/Monopoly.java
@@ -13,9 +13,9 @@ import java.util.Random;
 public class Monopoly {
 	private int nbMaisons = 32;
 	private int nbHotels = 12;
-	private ArrayList<Joueur> joueurs = new ArrayList<Joueur>();
-	public  Interface inter;
-        private ArrayList<Groupe> groupes = new ArrayList();
+	private ArrayList<Joueur> joueurs = new ArrayList<>();
+	private Interface inter;
+        private ArrayList<Groupe> groupes = new ArrayList<>();
         private HashMap<Integer, Carreau> carreaux;
         private int numJoueur;
         private LinkedList<CarteChance> carteChances = new LinkedList<>();
@@ -29,157 +29,165 @@ public class Monopoly {
                 initCarte();
 	}
         
-    public int lancerDe() {
-        Random jet = new Random();
-        int valDe = jet.nextInt(5)+1;
-        
-        return valDe;
-    }
-    
-    public int lancerDes(){
-        int de1;
-        int de2;
-        de1 = lancerDe();
-        de2 = lancerDe();
-        getCurrentPlayer().setDoubleDe(de1 == de2);
-        inter.afficherLancerDe(de1,de2);
-        return de1 + de2;
-        
-    }
-    
-    // Lance les des et le fais avancer normalement ou en prison
-    public void lancerDesEtAvancer(){
-        Joueur joueur = this.getCurrentPlayer();
-        int des;
-
-        des = lancerDes();
-        // Si il fait un double on incremente
-        if (joueur.getDoubleDe()) {joueur.incrNbDouble();}
-        
-        if (joueur.getNbDouble() == 3){
-            joueur.allerPrison();
-        } else {
-            joueur.avancer(des);
+        public int lancerDe() {
+            Random jet = new Random();
+            int valDe = jet.nextInt(5)+1;
+            return valDe;
         }
-        inter.afficherEtatJoueur(joueur);
-    }
-    
-    
-    
-    
-    public void sortirPrison() {
+
+        /**
+         * Fais deux jets de dés aléatoire.
+         * Et indique dans doubleDe si le joueur à fait un double
+         * @return La somme des dés
+         */
+        protected int lancerDes(){
+            int de1;
+            int de2;
+            de1 = lancerDe();
+            de2 = lancerDe();
+            getCurrentPlayer().setDoubleDe(de1 == de2);
+            inter.afficherLancerDe(de1,de2);
+            return de1 + de2;
+
+        }
         
-    }
-    
-    
-    
-    public LinkedList<CarteChance> getCarteChances(){
-        return this.carteChances;
-    }
-    public void setCarteChances(LinkedList<CarteChance> carteChances){
-        this.carteChances = carteChances;
-    }
-       
-    public LinkedList<CarteCaisse> getCarteCaisses(){
-        return this.carteCaisses;
-    }
-    public void setCarteCaisses(LinkedList<CarteCaisse> carteCaisses){
-        this.carteCaisses = carteCaisses;
-    }
-    
-    public Joueur getCurrentPlayer() {
-        return joueurs.get(numJoueur-1);
-    }
-    public void setCurrentPlayer(Joueur joueur) {
-        numJoueur = (joueurs.indexOf(joueur))+1;
-    }
 
-    public int getNbMaisons() {
-        return nbMaisons;
-    }
-    public void setNbMaisons(int nbMaisons) {
-        this.nbMaisons = nbMaisons;
-    }
+        /**
+         * Lance les des et fais avancer le joueur normalement ou en prison
+         * Affiche ensuite l'état du joueur
+         */
+            public void lancerDesEtAvancer(){
+            Joueur joueur = this.getCurrentPlayer();
+            int des;
 
-    public int getNbHotels() {
-        return nbHotels;
-    }
-    public void setNbHotels(int nbHotels) {
-        this.nbHotels = nbHotels;
-    }
+            des = lancerDes();
+            // Si il fait un double on incremente
+            if (joueur.getDoubleDe()) {joueur.incrNbDouble();}
 
-    public ArrayList<Joueur> getJoueurs() {
-        return joueurs;
-    }
-    public void setJoueurs(ArrayList<Joueur> joueurs) {
-        this.joueurs = joueurs;
-    }
+            if (joueur.getNbDouble() == 3){
+                joueur.allerPrison();
+            } else {
+                joueur.avancer(des);
+            }
+            inter.afficherEtatJoueur(joueur);
+        }
 
-    public Interface getInter() {
-        return inter;
-    }
-    public void setInter(Interface inter) {
-        this.inter = inter;
-    }
 
-    public ArrayList<Groupe> getGroupes() {
-        return groupes;
-    }
-    public void setGroupes(ArrayList<Groupe> groupes) {
-        this.groupes = groupes;
-    }
 
-    public HashMap<Integer, Carreau> getCarreaux() {
-        return carreaux;
-    }
-    public void setCarreaux(HashMap<Integer, Carreau> carreaux) {
-        this.carreaux = carreaux;
-    }
-	
-    public Carreau getCarreau(int position){
-        return carreaux.get(position);
-    }
+
+        public void sortirPrison() {
+
+        }
+
+
+
+        public LinkedList<CarteChance> getCarteChances(){
+            return this.carteChances;
+        }
+        public void setCarteChances(LinkedList<CarteChance> carteChances){
+            this.carteChances = carteChances;
+        }
+
+        public LinkedList<CarteCaisse> getCarteCaisses(){
+            return this.carteCaisses;
+        }
+        public void setCarteCaisses(LinkedList<CarteCaisse> carteCaisses){
+            this.carteCaisses = carteCaisses;
+        }
+
+        public Joueur getCurrentPlayer() {
+            return joueurs.get(numJoueur-1);
+        }
+        public void setCurrentPlayer(Joueur joueur) {
+            numJoueur = (joueurs.indexOf(joueur))+1;
+        }
+
+        public int getNbMaisons() {
+            return nbMaisons;
+        }
+        public void setNbMaisons(int nbMaisons) {
+            this.nbMaisons = nbMaisons;
+        }
+
+        public int getNbHotels() {
+            return nbHotels;
+        }
+        public void setNbHotels(int nbHotels) {
+            this.nbHotels = nbHotels;
+        }
+
+        public ArrayList<Joueur> getJoueurs() {
+            return joueurs;
+        }
+        public void setJoueurs(ArrayList<Joueur> joueurs) {
+            this.joueurs = joueurs;
+        }
+
+        public Interface getInter() {
+            return inter;
+        }
+        public void setInter(Interface inter) {
+            this.inter = inter;
+        }
+
+        public ArrayList<Groupe> getGroupes() {
+            return groupes;
+        }
+        public void setGroupes(ArrayList<Groupe> groupes) {
+            this.groupes = groupes;
+        }
+
+        public HashMap<Integer, Carreau> getCarreaux() {
+            return carreaux;
+        }
+        public void setCarreaux(HashMap<Integer, Carreau> carreaux) {
+            this.carreaux = carreaux;
+        }
+
+        public Carreau getCarreau(int position){
+            return carreaux.get(position);
+        }
         private void addJoueur (Joueur joueur){
             joueurs.add(joueur);
         }
-        
+
         public void nouveauJoueur(String nomJoueur){
             Joueur j = new Joueur(nomJoueur, getCarreau(1), this);
             addJoueur(j);
         }
-        
-        
+
+
         public void payer(Joueur payeur, Joueur receveur, int somme) {
             payeur.removeCash(somme);
             receveur.addCash(somme);
-            
+
         }
-        
-        
-        
+
+
+
         public void checkPerdu(Joueur joueur) {
             if (joueur.getCash() <= 0) {
                 this.removeJoueur(joueur);
             }
         }
-        
-        
+
+
         public void removeJoueur(Joueur joueur) {
             this.getJoueurs().remove(joueur);
         }
-        
-        
+
+
         //////// Creation du plateau /////      
-        
-	private void buildGamePlateau(String dataFilename)
-	{
-		try{
-			ArrayList<String[]> data = readDataFile(dataFilename, ",");
-			
-			//TODO: create cases instead of displaying
-			for(int i=0; i<data.size(); ++i){
-				String caseType = data.get(i)[0];
-				if(caseType.compareTo("P") == 0){
+
+        private void buildGamePlateau(String dataFilename)
+        {
+                try{
+                        ArrayList<String[]> data = readDataFile(dataFilename, ",");
+
+                        //TODO: create cases instead of displaying
+                        for(int i=0; i<data.size(); ++i){
+                                String caseType = data.get(i)[0];
+                                if(caseType.compareTo("P") == 0){
                                     // Recupération des différentes variables
                                         int num = Integer.parseInt(data.get(i)[1]);
                                         String nomC = data.get(i)[2];
@@ -198,61 +206,61 @@ public class Monopoly {
                                         loyer.add(mais3);
                                         loyer.add(mais4);
                                         loyer.add(hotel);
-                                        
+
                                         ProprieteAConstruire c = new ProprieteAConstruire(num,nomC,this,prix,loyer,grp);                                    
                                         carreaux.put(num,c);
-				}
-				else if(caseType.compareTo("G") == 0){
+                                }
+                                else if(caseType.compareTo("G") == 0){
                                         int num = Integer.parseInt(data.get(i)[1]);
                                         String nomC = data.get(i)[2];
                                         int prix = Integer.parseInt(data.get(i)[3]);
-                                        
+
                                         Gare c = new Gare(num,nomC,this,prix);
                                         carreaux.put(num,c);
-				}
-				else if(caseType.compareTo("C") == 0){
+                                }
+                                else if(caseType.compareTo("C") == 0){
                                         int num = Integer.parseInt(data.get(i)[1]);
                                         String nomC = data.get(i)[2];
                                         int prix = Integer.parseInt(data.get(i)[3]);
-                                        
+
                                         Compagnie c = new Compagnie(num,nomC,this,prix);
                                         carreaux.put(num,c);
-				}
-				else if(caseType.compareTo("CT") == 0){
+                                }
+                                else if(caseType.compareTo("CT") == 0){
                                         int num = Integer.parseInt(data.get(i)[1]);
                                         String nomC = data.get(i)[2];
-                                        
+
                                         CarreauTirage c = new CarreauTirage(num,nomC,this);
                                         carreaux.put(num,c);
-				}
-				else if(caseType.compareTo("CA") == 0){
+                                }
+                                else if(caseType.compareTo("CA") == 0){
                                         int num = Integer.parseInt(data.get(i)[1]);
                                         String nomC = data.get(i)[2];
                                         int montant = Integer.parseInt(data.get(i)[3]);
-                                        
+
                                         CarreauArgent c = new CarreauArgent(num,nomC,this,montant);
                                         carreaux.put(num,c);
-				}
-				else if(caseType.compareTo("CM") == 0){
+                                }
+                                else if(caseType.compareTo("CM") == 0){
                                         int num = Integer.parseInt(data.get(i)[1]);
                                         String nomC = data.get(i)[2];
-                                        
+
                                         CarreauTirage c = new CarreauTirage(num,nomC,this);
                                         carreaux.put(num,c);
-				}
-				else
-					System.err.println("[buildGamePleateau()] : Invalid Data type");
-			}
-			
-		} 
-		catch(FileNotFoundException e){
-			System.err.println("[buildGamePlateau()] : File is not found!");
-		}
-		catch(IOException e){
-			System.err.println("[buildGamePlateau()] : Error while reading file!");
-		}
-	}
-        
+                                }
+                                else
+                                        System.err.println("[buildGamePleateau()] : Invalid Data type");
+                        }
+
+                } 
+                catch(FileNotFoundException e){
+                        System.err.println("[buildGamePlateau()] : File is not found!");
+                }
+                catch(IOException e){
+                        System.err.println("[buildGamePlateau()] : Error while reading file!");
+                }
+        }
+
         private void initGroupe(){
             Groupe bleuFonce = new Groupe(CouleurPropriete.bleuFonce, 200, 200);
             Groupe orange    = new Groupe(CouleurPropriete.orange, 100, 100);
@@ -262,7 +270,7 @@ public class Monopoly {
             Groupe jaune     = new Groupe(CouleurPropriete.jaune, 150, 150);
             Groupe vert      = new Groupe(CouleurPropriete.vert, 200, 200);
             Groupe rouge     = new Groupe(CouleurPropriete.rouge, 150, 150);
-            
+
             groupes.add(bleuFonce);
             groupes.add(orange);
             groupes.add(mauve);
@@ -272,7 +280,7 @@ public class Monopoly {
             groupes.add(vert);
             groupes.add(rouge);
         }
-        
+
         private Groupe getGroupe(String couleur){
             for (Groupe grp : groupes){
                 if (grp.getCouleur().toString() == couleur){
@@ -281,7 +289,7 @@ public class Monopoly {
             }
             return null;
         }
-        
+
         private void initCarte(){
             Random res = new Random();
             LinkedList<CarteCaisse> carteCaissesTemp = new LinkedList();
@@ -299,20 +307,20 @@ public class Monopoly {
                 carteChances.add(carteChancesTemp.remove(res.nextInt(i)));
             }
         }
-	
-	private ArrayList<String[]> readDataFile(String filename, String token) throws FileNotFoundException, IOException
-	{
-		ArrayList<String[]> data = new ArrayList<String[]>();
-		
-		BufferedReader reader  = new BufferedReader(new FileReader(filename));
-		String line = null;
-		while((line = reader.readLine()) != null){
-			data.add(line.split(token));
-		}
-		reader.close();
-		
-		return data;
-	}
+
+        private ArrayList<String[]> readDataFile(String filename, String token) throws FileNotFoundException, IOException
+        {
+                ArrayList<String[]> data = new ArrayList<String[]>();
+
+                BufferedReader reader  = new BufferedReader(new FileReader(filename));
+                String line = null;
+                while((line = reader.readLine()) != null){
+                        data.add(line.split(token));
+                }
+                reader.close();
+
+                return data;
+        }
         
         
 }
